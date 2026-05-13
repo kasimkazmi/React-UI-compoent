16243eec83643f0a4bcdc101de30d9ed038690d6
kasimkazmi <kasimkazmi27@gmail.com>
2026-05-12 22:12:31 -0400

    chore: add magicui dependencies and tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -43,6 +43,9 @@ module.exports = {
           foreground: "hsl(var(--card-foreground))",
         },
       },
+      backgroundImage: {
+        "grid-white": "url(\"data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 32 32' width='32' height='32' fill='none' stroke='rgb(255 255 255 / 0.04)'%3e%3cpath d='M0 .5H31.5V32'/%3e%3c/svg%3e\")",
+      },
     },
   },
   plugins: [require("@tailwindcss/typography")],
